759e87f97cca897eabfa8d4465cf371fb30b78a6
Mayank Mehra <mayankmehra13.04@gmail.com>
2024-03-21 21:48:18 +0530

    Question 5

--- question05.java
+++ question05.java
@@ -15,7 +15,7 @@ public class question05 {
                 }
                 
                 Arrays.sort(arr);
-                for (int i = 1; i <= arr.length - 2; i += 2) {
+                for (int i = 1; i < arr.length - 1; i += 2) {
                         int temp = arr[i];
                         arr[i] = arr[i + 1];
                         arr[i + 1] = temp;
