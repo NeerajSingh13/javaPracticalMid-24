8022daa4efdb8f90ef0bfb868a8670d35dcf1d6c
Mayank Mehra <mayankmehra13.04@gmail.com>
2024-03-21 15:07:16 +0530

    sa

--- question02.java
+++ question02.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.sound.midi.Soundbank;
-
 public class question02 {
         public static void main(String[] args) {
                 Scanner input = new Scanner(System.in);
